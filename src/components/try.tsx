2fe3777e79fdd88bbe4825389b8321735bd4d726
Collin Kleest <collinkleest@gmail.com>
2025-03-22 16:39:04 -0400

    feat(try): update try to use dynamic value for proxy

--- src/components/try.tsx
+++ src/components/try.tsx
@@ -1,37 +1,53 @@
-import { Button, Code, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Button, Code, Field, Input, Text } from "@chakra-ui/react";
+import React, { useRef, useState } from "react";
 
 export const Try = () => {
-    const [response, setResponse] = useState('')
-    const [clicks, setClicks] = useState(0)
-    
-    const fetchExample = () => {
-        setClicks(clicks + 1)
-        if (clicks > 3) {
-            return
-        }
-        fetch('https://api.liteproxy.collinkleest.com/proxy?url=https://jsonplaceholder.typicode.com/comments?postId=1').then((response) => {
-            return response.json();
-        }).then((json) => {
-            setResponse(JSON.stringify(json))
-        })
-    }
+  const DEFAULT_VALUE =
+    "https://jsonplaceholder.typicode.com/comments?postId=1";
+  const [response, setResponse] = useState("");
+  const [inputValue, setInputValue] = useState<string>(DEFAULT_VALUE);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const fetchExample = () => {
+    const proxyUrl = inputRef.current?.value;
+    fetch(`https://api.liteproxy.collinkleest.com/proxy?url=${proxyUrl}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setResponse(JSON.stringify(json));
+      });
+  };
 
   return (
     <>
-      <Button onClick={fetchExample} my={2}size='sm'>Try it now</Button>
-
       <Text>
         <Code>
-          https://api.liteproxy.collinkleest.com/proxy?url=https://jsonplaceholder.typicode.com/comments?postId=1
+          https://api.liteproxy.collinkleest.com/proxy?url={"<"} desired url you
+          want to proxy to {">"}
         </Code>
-
       </Text>
 
-      {response && <Code variant={'outline'}>
-        {response}
-      </Code>
-}
+      <Field.Root required>
+        <Field.Label>
+          Proxy Url <Field.RequiredIndicator />
+        </Field.Label>
+        <Input
+          placeholder={DEFAULT_VALUE}
+          value={inputValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setInputValue(e.target.value)
+          }
+          ref={inputRef}
+        />
+        <Field.HelperText>Enter a valid url.</Field.HelperText>
+      </Field.Root>
+
+      <Button onClick={fetchExample} my={2} size="sm">
+        Try it now
+      </Button>
+
+      {response && <Code variant={"outline"}>{response}</Code>}
     </>
   );
 };
